606707c80f7e0ac81debad4b50854647cffddddf
Risto "Dilaz" Viitanen <risto.viitanen@iki.fi>
2022-11-26 14:11:15 +0200

    Remove unnecessary event-vote module

--- src/event/event-vote/dto/update-event-vote.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateEventVoteDto } from './create-event-vote.dto';
-
-export class UpdateEventVoteDto extends PartialType(CreateEventVoteDto) {}
--- src/event/event-vote/event-vote.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { EventVoteService } from './event-vote.service';
-import { EventVoteController } from './event-vote.controller';
-
-@Module({
-  controllers: [EventVoteController],
-  providers: [EventVoteService]
-})
-export class EventVoteModule {}
